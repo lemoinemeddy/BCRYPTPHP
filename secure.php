d3f70aba2e1c61ebc885c0a5190344d30a99735d
lemoinemeddy <53619610+lemoinemeddy@users.noreply.github.com>
2019-08-05 02:04:37 +0200

    Update secure.php

--- secure.php
+++ secure.php
@@ -7,9 +7,14 @@ public function try_signin($user, $mdp){
   if($mdp <= 72){
     $hash = password_hash($mdp, PASSWORD_BCRYPT);
     $hashbdd = '';
-    return password_verify($hash_bdd, $hash); // return TRUE or FALSE
-  }
-  else{
+    $response = password_verify($hash_bdd, $hash); // return TRUE or FALSE
+    $payload = $user + time();
+    $token = password_hash($payload, PASSWORD_BCRYPT)
+    return '{
+    "auth":'.$response.',
+    "token":'.$token.'
+}';
+}else{
     return 'needed < 72';
   }
 }
